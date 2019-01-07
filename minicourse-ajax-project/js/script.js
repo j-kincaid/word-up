7807f6987f93d8ca0910e0c15baee32c0d09bb92
Jessica Kincaid <4j.kincaid@gmail.com>
2019-01-07 12:07:18 -0600

    first commit

---
+++ minicourse-ajax-project/js/script.js
@@ -0,0 +1,27 @@
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    var streetStr = $('#street').val();
+    var cityStr = $('city').val();
+    var address = streetStr + ', ' + cityStr;
+
+    $greeeting.text('So, you want to live at ' + address + '?');
+
+
+    // load streetview
+
+    // YOUR CODE GOES HERE!
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
